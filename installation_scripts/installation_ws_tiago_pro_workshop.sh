0d0d55a65bb71c78aa18d823b1c31b00de97577e
andreacapodacqua <andrea.capodacqua@pal-robotics.com>
2025-10-28 00:06:09 +0100

    update year

--- installation_scripts/installation_ws_tiago_pro_workshop.sh
+++ installation_scripts/installation_ws_tiago_pro_workshop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) 2021 PAL Robotics S.L. All rights reserved.
+# Copyright (c) 2025 PAL Robotics S.L. All rights reserved.
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are met:
