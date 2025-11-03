f05f1b80569d26aa9ab707a6da4c1df4649ce74b
andrycapod <andreacapodacqua@gmail.com>
2025-11-03 11:32:31 +0100

    cosmetic

--- exercises/exercise_4/target_detection_tutorial/src/dummy_target_detector.cpp
+++ exercises/exercise_4/target_detection_tutorial/src/dummy_target_detector.cpp
@@ -11,8 +11,11 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
+// 
+// Author: Andrea Capodacqua
+// Author: Lorenzo Ferrini
 // Author: Martina Annicelli
+
 
 /**
  * @file dummy_target_detector.cpp
